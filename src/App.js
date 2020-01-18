77d0f0a8b25ca6f0b4c9a0146f979bfb5a1ed9bc
Nguyen Thi Cam To <camto2831997@gmail.com>
2020-01-18 16:04:12 +0700

    adding redirect after user signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -41,15 +41,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SigninAndSignupPage} />
+          <Route exact path='/signin' 
+                render={() => 
+                this.props.currentUser ? 
+                (<Redirect to='/' />) : 
+                (<SigninAndSignupPage/>)}/>
         </Switch>
       </Fragment>
     )
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(App);
