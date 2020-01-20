62e6242780eafd58806cc51b4ec6c8ecf560a63c
Nguyen Thi Cam To <camto2831997@gmail.com>
2020-01-20 23:05:32 +0700

    added addItem and removeItem functionalities to checkout-item component

--- src/components/checkout-item/checkout-item.js
+++ src/components/checkout-item/checkout-item.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearCartItem, addItem, removeItem } from '../../redux/cart/cart.action';
 
 import './checkout-item.scss';
 
-const CheckoutItem = ({cartItem: {imageUrl, name, quantity, price}}) => (
-    <div className='checkout-item'>
-        <div className='checkout-item__image'>
-            <img src={imageUrl} alt={name} />
+const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
+    const { imageUrl, name, quantity, price } = cartItem;
+    return (
+        <div className='checkout-item'>
+            <div className='checkout-item__image'>
+                <img src={imageUrl} alt={name} />
+            </div>
+            <div className='checkout-item__name'>{name}</div>
+            <div className='checkout-item__quantity'>
+                <span className='checkout-item__quantity-arrow' onClick={() => removeItem(cartItem)}>&#10094;</span>
+                <span className='checkout-item__quantity-value'>{quantity}</span>
+                <span className='checkout-item__quantity-arrow' onClick={() => addItem(cartItem)}>&#10095;</span>
+            </div>
+            <div className='checkout-item__price'>${price}</div>
+            <div className='checkout-item__remove' onClick={() => clearItem(cartItem)}>&#10005;</div>
         </div>
-        <span className='checkout-item__name'>{name}</span>
-        <span className='checkout-item__quantity'>{quantity}</span>
-        <span className='checkout-item__price'>${price}</span>
-        <span className='checkout-item__remove'>&#10005;</span>
-    </div>
-);
+    )
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearCartItem(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
